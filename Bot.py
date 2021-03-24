c95f2ca6e2f451b0aff3e3aa71039dd67829fab8
AnimatorJoe <josephjin7@gmail.com>
2021-03-23 18:56:07 -0700

    using full xpaths instead

--- Bot.py
+++ Bot.py
@@ -3,6 +3,7 @@ from time import sleep
 from login_cred import fb_username, fb_password
 from heuristics import always_true, filter_blocked, favored_only, favored_and_not_blocked
 from math import inf
+from config import verbose
 import random
 import sys
 
@@ -14,16 +15,17 @@ class Bot:
     def fb_login(self):
         # load webpage
         self.driver.get("http://www.tinder.com")
+        self.driver.set_window_size(960, 720)
 
         sleep(2)
 
         # click on login button
-        login_btn = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/button')
+        login_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/button')
         login_btn.click()
         sleep(2)
 
         # open fb login popup
-        fb_login_option = self.driver.find_element_by_xpath('//*[@id="t--149300558"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+        fb_login_option = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/div[3]/span/div[2]/button')
         fb_login_option.click()
         self.driver.switch_to.window(self.driver.window_handles[1])
 
@@ -41,16 +43,15 @@ class Bot:
         fb_login_btn.click()
 
         self.driver.switch_to.window(self.driver.window_handles[0])
-        self.driver.set_window_size(960, 720)
 
     # swipe right
     def swipe_right(self):
-        right_btn = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button')
+        right_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button')
         right_btn.click()
     
     # swipe left
     def swipe_left(self):
-        left_btn = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[2]/button')
+        left_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[2]/button')
         left_btn.click()
 
     # swipe right inf times unconditional
@@ -73,45 +74,68 @@ class Bot:
             rand_wait = random.uniform(0, 1.5)
             sleep(rand_wait)
 
-            p = self.get_profile()
+            favorable = True
+            p = {}
 
             try:
-                _ = self.swipe_right() if heuristic(p) else self.swipe_left()
-                count += 1
+                p = self.get_profile()
+                favorable = heuristic(p)
             except Exception:
-                self.close_popup('//*[@id="t--149300558"]/div/div/div[2]/button[2]')
+                pass
+
+            try:
+                if favorable:
+                    if (verbose):
+                        print("swipped right on")
+                        print(p)
+                    self.swipe_right()
+                else:
+                    if (verbose):
+                        print("swipped left on")
+                        print(p)
+                    self.swipe_left()
+                count += 1
+            except Exception as e:
+                print('Unable to swipe:')
+                if (verbose):
+                    print(str(e))
+
+                self.close_popup('//*[@id="t-1483503441"]/div/div/div[2]/button[2]')
+
 
     # close popup
     def close_popup(self, xpath):
-        btn = self.driver.find_element_by_xpath('//*[@id="t--149300558"]/div/div/div[2]/button[2]')
+        btn = self.driver.find_element_by_xpath(xpath)
         btn.click()
 
     # returns profile of current profile as a dict
     def get_profile(self):
         # expand profile
-        expand_btn = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[1]/div[3]/div[3]/button')
+        expand_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[1]/div[3]/div[3]/button')
         expand_btn.click()
 
         # read profile
         dict = {}
         try:
-            bio_container = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[2]/div')
+            bio_container = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[2]/div')
             dict['bio'] = bio_container.text
         except Exception:
             pass
         
         try:
-            info_container = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div')
+            info_container = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]')
             dict['info'] = info_container.text
         except Exception:
             pass
         
-        # print("parsed profile entry")
-        # print(dict)
+        print("parsed profile entry")
+        print(dict)
 
         # close profile
-        close_btn = self.driver.find_element_by_xpath('//*[@id="t-1890905246"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[1]/span/a')
+        close_btn = self.driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[1]/span/a')
         close_btn.click()
+
+        sleep(0.5)
 
         return dict
 
